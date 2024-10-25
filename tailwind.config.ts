e353e35c0b41e121374a4eead422ae7316924baf
ChiSamuel651 <chijoe0306@gmail.com>
2024-10-25 13:29:48 +0100

    app setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+// import {nextui} from '@nextui-org/theme';
+import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -5,6 +7,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -14,6 +17,23 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  //
+  plugins: [nextui({
+    themes: {
+      light: {
+        // ...
+        colors: {
+          primary:{
+            DEFAULT:"#5253A3"
+          }
+        },
+      },
+      dark: {
+        // ...
+        colors: {},
+      },
+      // ... custom themes
+    },
+  })],
 };
 export default config;
