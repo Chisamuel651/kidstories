3a89e9e29e69670b792f3b39b9a3a42e53c65698
ChiSamuel651 <chijoe0306@gmail.com>
2024-10-29 14:34:43 +0100

    create story page setup

--- app/create-story/page.jsx
+++ app/create-story/page.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function CreateStory() {
   return (
-    <div>
-      
+    <div className='p-10 md:px-20 lg:px-40'>
+      <h2 className='font-extrabold text-[70px] text-primary text-center'>CREATE YOUR STORY</h2>
+
+      <p className='text-2xl text-primary text-center'>With the power of our AI, create stories like never before. Watch as your ideas turn into amazing adventures, one story at a time.</p>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-10 mt-14'>
+        {/* story subject */}
+        {/* story type */}
+        {/* age group */}
+        {/* image style */}
+      </div>
     </div>
   )
 }
--- app/provider.tsx
+++ app/provider.tsx
@@ -1,11 +1,14 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import { NextUIProvider } from '@nextui-org/react'
 import React from 'react'
+import Header from './_components/Header'
 
 function Provider({children}: {children: React.ReactNode}) {
   return (
     <ClerkProvider>
       <NextUIProvider>
+        {/* header */}
+        <Header />
         {children}
       </NextUIProvider>
     </ClerkProvider>
